2179f1c9e2196f91a43bb8f9d4b30f32a662cf7e
Lakshay <lakshay21060@iiitd.ac.in>
2024-03-29 13:57:42 +0530

    added nonce verification on client side

--- pkda/client.c
+++ pkda/client.c
@@ -23,7 +23,8 @@ int main(int argc, char **argv)
 	assert(connect(client_sock, (struct sockaddr *)&server_addr,
 				   sizeof(server_addr)) >= 0 && "Error: connection failed");
 
-	char *request = create_request_public_key_message("0", "1", "nonce");
+	char *nonce_sent = "the one who lays dormant...";
+	char *request = create_request_public_key_message("0", "1", nonce_sent);
 	printf("Info: REQUEST START\n%s\nInfo: REQUEST END\n", request);
 	char *encrypted_request = encrypt_request(request);
 	assert(send(client_sock, encrypted_request, slen(encrypted_request), 0)
@@ -36,7 +37,21 @@ int main(int argc, char **argv)
 		printf("Info: response received\n");
 		buffer[bytes_received] = 0;
 		char *response = decrypt_response(buffer);
-		printf("Info: RESPONSE START\n%s\nInfo: RESPONSE END\n", response);
+		json_error_t error;
+		json_t *json_obj = json_loads(response, 0, &error);
+		true_unless_kill(json_obj != NULL, "failed to parse json");
+		true_unless_kill(json_is_object(json_obj) ,
+						 "received response is not a json object");
+				
+		printf("Info: RESPONSE START\n");
+		print_json_object(json_obj);
+		printf("Info: RESPONSE END\n");
+
+		char *nonce_recv = json_get_string(json_obj, "nonce");
+		if (scmp(nonce_sent, nonce_recv))
+			printf("Info: successfully verified response\n");
+		else printf("Info: failed response verification\n");
+
 	}
     close(client_sock);
     return 0;
